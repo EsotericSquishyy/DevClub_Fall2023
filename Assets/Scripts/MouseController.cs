639359a58325c36e75a88bc27cb2a93c8abe3cac
FishyySquishyy <jesselcobb@gmail.com>
2023-11-01 12:30:53 -0700

    Followed tutorial on making tiles selectable

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MouseController : MonoBehaviour
+{
+    void LateUpdate(){
+        var focusedTileHit = GetFocusedOnTile();
+
+        if(focusedTileHit.HasValue){
+            GameObject overlayTile = focusedTileHit.Value.collider.gameObject;
+
+            transform.position = overlayTile.transform.position;
+            gameObject.GetComponent<SpriteRenderer>().sortingOrder = overlayTile.GetComponent<SpriteRenderer>().sortingOrder;
+
+            if(Input.GetMouseButtonDown(0)){
+                overlayTile.GetComponent<TileSelect>().ShowTile();
+            }
+        }
+    }
+
+    public RaycastHit2D? GetFocusedOnTile(){
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2d = new Vector2(mousePos.x, mousePos.y);
+
+        RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos2d, Vector2.zero);
+
+        if(hits.Length > 0){
+            return hits.OrderByDescending(i => i.collider.transform.position.z).First();
+        }
+
+        return null;
+    }
+}
